53c882d6b31e51ba61115a793d8df81f0a801d76
bedcar <ruru@oeoe.com>
2022-06-06 21:51:39 +0200

    now saloragno days is better and the two uses the same formula

--- zcrocco/spiders/SaloRagno.py
+++ zcrocco/spiders/SaloRagno.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 import scrapy
-from zcrocco.items import Serata
 
 class SaloRagno(scrapy.Spider):
 	name = 'saloragno'
@@ -9,7 +8,7 @@ class SaloRagno(scrapy.Spider):
 	]
 
 	def parse(self, response):
-		lista=response.xpath('//a[@class="col-xs-12 nopadding item_related_link event_box_item special"]/@href').extract()
+		lista=response.xpath('//a[@class="col-xs-12 nopadding item_related_link event_box_item base"]/@href').extract()
 		print(len(lista))
 		for pagina in lista:
 			if "fuorisalone.it" in pagina:
@@ -28,43 +27,17 @@ class SaloRagno(scrapy.Spider):
 		if not a:
 			print('nulla')
 		else:
-			serata=Serata()
-			serata['nome']=response.xpath('//h1[@class="event_title strong col-xs-12 col-md-9 col-lg-8 nopadding"]/text()').get()
-			serata['luogo']=response.xpath('//a[@class="link-indirizzo-location"]/text()').get()
-			try:
-				serata['lunedi']=response.xpath("/html/body/div/main/article/div[2]/div/div/div[2]/div[1]/div[2]/text()").extract()[1].encode("utf-8").strip()
-				serata['lunediora']=response.xpath("/html/body/div/main/article/div[2]/div/div/div[2]/div[1]/div[2]/span/text()").get().encode("utf-8").strip()
-			except:
-				serata['lunedi']=""
-				serata['lunediora']=""
-			try:
-				serata['martedi']=response.xpath("/html/body/div/main/article/div[2]/div/div/div[2]/div[2]/div[2]/text()").extract()[1].encode("utf-8").strip()
-				serata['martediora']=response.xpath("/html/body/div/main/article/div[2]/div/div/div[2]/div[2]/div[2]/span/text()").get().encode("utf-8").strip()
-			except:
-				serata['martedi']=""
-				serata['martediora']=""
-			try:
-				serata['mercoledi']=response.xpath("/html/body/div/main/article/div[2]/div/div/div[2]/div[3]/div[2]/text()").extract()[1].encode("utf-8").strip()
-				serata['mercolediora']=response.xpath("/html/body/div/main/article/div[2]/div/div/div[2]/div[3]/div[2]/span/text()").get().encode("utf-8").strip()
-			except:
-				serata['mercoledi']=""
-				serata['mercolediora']=""
-			try:
-				serata['giovedi']=response.xpath("/html/body/div/main/article/div[2]/div/div/div[2]/div[4]/div[2]/text()").extract()[1].encode("utf-8").strip()
-				serata['giovediora']=response.xpath("/html/body/div/main/article/div[2]/div/div/div[2]/div[4]/div[2]/span/text()").get().encode("utf-8").strip()
-			except:
-				serata['giovedi']=""
-				serata['giovediora']=""
-			try:
-				serata['venerdi']=response.xpath("/html/body/div/main/article/div[2]/div/div/div[2]/div[5]/div[2]/text()").extract()[1].encode("utf-8").strip()
-				serata['venerdiora']=response.xpath("/html/body/div/main/article/div[2]/div/div/div[2]/div[5]/div[2]/span/text()").get().encode("utf-8").strip()
-			except:
-				serata['venerdi']=""
-				serata['venerdiora']=""
-			try:
-				serata['sabato']=response.xpath("/html/body/div/main/article/div[2]/div/div/div[2]/div[6]/div[2]/text()").extract()[1].encode("utf-8").strip()
-				serata['sabatoora']=response.xpath("/html/body/div/main/article/div[2]/div/div/div[2]/div[6]/div[2]/span/text()").get().encode("utf-8").strip()
-			except:
-				serata['sabato']=""
-				serata['sabatoora']=""
-			yield serata
+			result={
+				"name":[],
+				"cocktails":[],
+				"address":[]}
+			result['name']=response.xpath('//h1[@class="event_title strong col-xs-12 col-md-9 col-lg-8 nopadding"]/text()').get()
+			result['address']=response.xpath('//a[@class="link-indirizzo-location"]/text()').get()
+			giorni = response.css(".giorno_palinsesto")
+			for giorno in giorni:
+				if "cocktail" in giorno.extract():
+					data = (giorno.xpath("div[@class='data_palinsesto']/text()").extract()[1].strip())
+					ora = (giorno.xpath("div[@class='ora_palinsesto']/span/text()").extract_first().strip())
+					data_ora= data+" "+ora
+					result["cocktails"].append(data_ora)
+			yield result
